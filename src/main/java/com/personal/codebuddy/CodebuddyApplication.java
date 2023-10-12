3ae2854b3b4eead3bf3fd6d73aaff8be287ffa98
Matthew Chon <matthewhchon@gmail.com>
2023-10-12 14:57:26 -0400

    first commit

---
+++ src/main/java/com/personal/codebuddy/CodebuddyApplication.java
@@ -0,0 +1,13 @@
+package com.personal.codebuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodebuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodebuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/codebuddy/CodebuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.codebuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodebuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
