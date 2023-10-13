3cc002e3cd6ac3adfcd0a7061bd27fcf989b0f81
Matthew Chon <matthewhchon@gmail.com>
2023-10-12 23:12:37 -0400

    created page outline

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -3,7 +3,15 @@ const ReactDOM = require('react-dom');
 
 class App extends React.Component {
 	render() {
-		return null
+		return <div id="app-container">
+			<div id="content-container">
+				<nav id="environment-settings-container"></nav>
+				<div id="problem-statement-container">
+					<div id="problem-category-container"></div>
+					<div id="code-editor"></div>
+				</div>
+			</div>
+		</div>
 	}
 }
 ReactDOM.render(<App />, document.getElementById('react'));
